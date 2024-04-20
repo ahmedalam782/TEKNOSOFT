1235201c514f9171116a2ba03f3363182c0553c6
ahmedmohamed <ahmedalam4887482@gmail.com>
2024-04-20 14:54:48 +0200

    create BMI Calculator and home screen and result Screen and use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:bmi_calculator/Screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const BMICalculator());
 }
 
